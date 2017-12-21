d5aa5ff0c40c539f9cf2548289fafe3e95c14f42
KaiN <kain@piwnica.ws>
2017-12-21 16:36:51 +0100

    Fixed hangups during projectile creation

--- src/gamestates/game/projectile.c
+++ src/gamestates/game/projectile.c
@@ -73,15 +73,18 @@ tProjectile *projectileCreate(
 ) {
 	// Find free projectile
 	tProjectile *pProjectile = 0;
-	for(FUBYTE i = s_fubPrevProjectileAdded+1; i != s_fubPrevProjectileAdded; ++i) {
+	FUBYTE i = s_fubPrevProjectileAdded+1;
+	do {
 		if(i == s_fubProjectileMaxCount)
 			i = 0;
+		logWrite("Checking %hhu\n", i);
 		if(s_pProjectiles[i].ubType == PROJECTILE_TYPE_OFF) {
 			pProjectile = &s_pProjectiles[i];
 			s_fubPrevProjectileAdded = i;
+			logWrite("Added projectile at index %hu\n", i);
 			break;
 		}
-	}
+	} while(i++ != s_fubPrevProjectileAdded);
 	if(!pProjectile)
 		return 0;
 
