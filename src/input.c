4cc4e7d14746727e7077849cf567a04dff43a624
KaiN <kain@piwnica.ws>
2018-03-09 18:00:02 +0100

    Simplified input

--- src/input.c
+++ src/input.c
@@ -12,21 +12,9 @@ void inputOpen() {
 }
 
 void inputProcess() {
-	ULONG ulWindowSignal;
-	ULONG ulSignals;
-	struct IntuiMessage *pMsg;
-
-	ulWindowSignal = 1L << g_sWindowManager.pWindow->UserPort->mp_SigBit;
-	ulSignals = SetSignal(0L, 0L);
-
 	joyProcess();
 	mouseProcess();
-
-	if (!(ulSignals & ulWindowSignal))
-		return;
-	while (pMsg = (struct IntuiMessage *) GetMsg(g_sWindowManager.pWindow->UserPort)) {
-		ReplyMsg((struct Message *) pMsg);
-	}
+	keyProcess();
 }
 
 void inputClose() {
