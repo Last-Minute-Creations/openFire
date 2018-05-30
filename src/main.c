b1cd2963e219ca1c89d1da727989ef79c30c5787
KaiN <kain@piwnica.ws>
2018-05-30 16:17:34 +0200

    Simplified main.c

--- src/main.c
+++ src/main.c
@@ -1,44 +1,21 @@
 #include "main.h"
-#include <stdlib.h>
-#include <ace/types.h>
-#include <ace/managers/memory.h>
-#include <ace/managers/log.h>
-#include <ace/managers/timer.h>
-#include <ace/managers/system.h>
-#include <ace/managers/blit.h>
-#include <ace/managers/copper.h>
+#include <ace/generic/main.h>
 #include <ace/managers/game.h>
 
 #include "config.h"
 #include "input.h"
 #include "gamestates/precalc/precalc.h"
 
-int main(void) {
-	systemCreate();
-	memCreate();
-	logOpen();
-	timerCreate();
-
-	blitManagerCreate();
-	copCreate();
-
+void genericCreate(void) {
 	inputOpen();
-	gameCreate();
 	gamePushState(precalcCreate, precalcLoop, precalcDestroy);
-	while (gameIsRunning()) {
-		timerProcess();
-		inputProcess();
-		gameProcess();
-	}
-	gameDestroy();
+}
+
+void genericProcess(void) {
+	inputProcess();
+	gameProcess();
+}
+
+void genericDestroy(void) {
 	inputClose();
-
-	copDestroy();
-	blitManagerDestroy();
-
-	timerDestroy();
-	logClose();
-	memDestroy();
-	systemDestroy();
-	return EXIT_SUCCESS;
 }
